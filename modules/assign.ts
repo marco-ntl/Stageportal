02dc20a74ffe8b1509dc842bd5a7647e27f40250
marco.ntl <marco.ntl@protonmail.com>
2022-03-20 03:07:14 +0100

    Renommé helpers en api

--- modules/assign.ts
+++ modules/assign.ts
@@ -3,7 +3,7 @@ import { Prompt } from "../types/prompt";
 import { Argument, IDictionary, IModule } from "../types/IModule";
 import { prompts, puppeteer } from "..";
 import { Console } from "console";
-import { ServicePortal } from "../helpers/ServicePortal";
+import { ServicePortal } from "../API/ServicePortal";
 
 /*const ARGUMENT_PC:Argument = {
     name: 'PC',
